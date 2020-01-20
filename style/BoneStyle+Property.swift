f16af48bb30b1e5ed0e9886300c69d83c71d432b
Andrea Bellotto <andreabellotto88@gmail.com>
2020-01-20 12:40:50 +0100

    updated bone

--- style/BoneStyle+Property.swift
+++ style/BoneStyle+Property.swift
@@ -1,5 +1,5 @@
 //
-//  {{name | firstUpperCase}}+Property.swift
+//  {{name | firstUppercase}}+Property.swift
 //  
 //
 //  Created by Andrea Bellotto on 14/01/2020.
@@ -8,19 +8,19 @@
 import UIKit
 import DressCode
 
-public extension {{name | firstUpperCase}} where View:UILabel{
+public extension {{name | firstUppercase}} where View:UILabel{
     func style(for theme:Themeable) -> Property<View> {
         return .empty() // comment this line and style as you wish
     }
 }
 
-public extension {{name | firstUpperCase}} where View:UIButton{
+public extension {{name | firstUppercase}} where View:UIButton{
     func style(for theme:Themeable) -> Property<View> {
         return .empty() // comment this line and style as you wish
     }
 }
 
-public extension {{name | firstUpperCase}} where View:UITextField{
+public extension {{name | firstUppercase}} where View:UITextField{
     func style(for theme:Themeable) -> Property<View> {
         return .empty() // comment this line and style as you wish
     }
--- style/BoneStyle.swift
+++ style/BoneStyle.swift
@@ -1,7 +1,7 @@
 import UIKit
 import DressCode
 
-public enum {{name | firstUpperCase}}<View>:String, CaseIterable, StylableView{
+public enum {{name | firstUppercase}}<View>:String, CaseIterable, StylableView{
     
     public func style(for theme: Themeable) -> Property<View> {
         return .empty()
@@ -9,37 +9,37 @@ public enum {{name | firstUpperCase}}<View>:String, CaseIterable, StylableView{
 }
 
 public extension Property {
-    static func theme(for styleView:{{name | firstUpperCase}}<Element>, forTheme theme:Themeable) -> Property<Element>{
+    static func theme(for styleView:{{name | firstUppercase}}<Element>, forTheme theme:Themeable) -> Property<Element>{
         return styleView.style(for: theme)
     }
 }
 
 public extension Stylable{
-    func applyStyleView(_ styleView:{{name | firstUpperCase}}<Self>, for theme:Themeable){
+    func applyStyleView(_ styleView:{{name | firstUppercase}}<Self>, for theme:Themeable){
         setStyle(styleView.style(for: theme))
     }
 }
 
-public extension {{name | firstUpperCase}} where View:UILabel{
+public extension {{name | firstUppercase}} where View:UILabel{
     func applyStyleView(for theme:Themeable, to views:View...){
         views.forEach({$0.setStyle(self.style(for: theme))})
     }
 }
 
-public extension {{name | firstUpperCase}} where View:UIButton{
+public extension {{name | firstUppercase}} where View:UIButton{
     func applyStyleView(for theme:Themeable, to views:View...){
         views.forEach({$0.setStyle(self.style(for: theme))})
     }
 }
 
 public extension UILabel{
-    func applyStyleView(_ styleView:{{name | firstUpperCase}}<UILabel>, for theme:Themeable){
+    func applyStyleView(_ styleView:{{name | firstUppercase}}<UILabel>, for theme:Themeable){
         setStyle(styleView.style(for: theme))
     }
 }
 
 public extension UIButton{
-    func applyStyleView(_ styleView:{{name | firstUpperCase}}<UIButton>, for theme:Themeable){
+    func applyStyleView(_ styleView:{{name | firstUppercase}}<UIButton>, for theme:Themeable){
         setStyle(styleView.style(for: theme))
     }
 }
