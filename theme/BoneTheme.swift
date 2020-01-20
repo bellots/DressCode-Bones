21fec9e40bf72e96f1ac9d4da6b01b4aa1e811c2
Andrea Bellotto <andreabellotto88@gmail.com>
2020-01-20 12:53:44 +0100

    added theme bone

---
+++ theme/BoneTheme.swift
@@ -0,0 +1,7 @@
+public enum {{ name | firstUppercase}}:Themeable{
+    
+    public static var defaultTheme: Themeable{
+        // define here a default for your theme return Self.light
+    }
+        
+}
